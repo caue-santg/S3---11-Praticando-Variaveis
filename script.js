fd9691c09fbbcb43e0afec8d558148b406e40e66
cauesantg <caue_paraty@hotmail.com>
2022-06-16 13:22:43 -0300

    Projeto inicial

---
+++ script.js
@@ -0,0 +1,10 @@
+let nome = "Cauê"
+let sobrenome = "dos Santos"
+let anoNascimento = 1992
+let cidade = "Paraty"
+
+let anoAtual  = 2022
+let idade = anoAtual - anoNascimento 
+
+
+console.log(`Eu sou o ${nome } ${sobrenome} tenho ${idade} anos e moro em ${cidade}`)
